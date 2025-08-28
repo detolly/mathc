6a589292bd606435f2618e334a7c6e2416cb4c2f
detolly <thomas@detolly.no>
2025-08-28 15:56:26 +0200

    constant folding
    this only works with commutative and associative operations for now.

--- mathc/main.cpp
+++ mathc/main.cpp
@@ -9,50 +9,6 @@
 #include <parser.hpp>
 #include <token.hpp>
 
-struct printer
-{
-    void operator()(const mathc::op_node& op)
-    {
-        std::print(stderr, "(");
-
-        if (op.left.get())
-            std::visit(printer{}, *op.left);
-
-        std::print(stderr, "{}", operation_type_to_string(op.type));
-
-        if (op.right.get())
-            std::visit(printer{}, *op.right);
-
-        std::print(stderr, ")");
-    }
-
-    void operator()(const mathc::constant_node& op)
-    {
-        std::print(stderr, "{}", op.value);
-    }
-
-    void operator()(const mathc::symbol_node& op)
-    {
-        std::print(stderr, "{}", op.value);
-    }
-
-    void operator()(const mathc::function_call_node& op)
-    {
-        std::print(stderr, "{}(", op.function_name);
-        for(auto i = 0u; i < op.arguments.size(); i++) {
-            const auto& argument = op.arguments[i];
-            std::visit(printer{}, argument);
-            if (i != op.arguments.size() - 1)
-                std::print(stderr, ", ");
-        }
-        std::print(stderr, ")");
-    }
-};
-
-static void print_tree(const mathc::node& root_node)
-{
-    std::visit(printer{}, root_node);
-}
 
 [[maybe_unused]]
 consteval static auto evaluate(const std::string_view source)
--- mathc/simplify.hpp
+++ mathc/simplify.hpp
@@ -4,6 +4,7 @@
 #include <variant>
 
 #include <common.hpp>
+#include <print.hpp>
 #include <functions.hpp>
 #include <node.hpp>
 #include <number.hpp>
@@ -14,6 +15,22 @@ namespace mathc
 
 // Implementation
 
+constexpr inline node operate(number a, number b, operation_type type)
+{
+    switch(type) {
+        case operation_type::mul:
+            return make_node<constant_node>(a * b);
+        case operation_type::div:
+            return make_node<constant_node>(a / b);
+        case operation_type::add:
+            return make_node<constant_node>(a + b);
+        case operation_type::sub:
+            return make_node<constant_node>(a - b);
+        case operation_type::exp:
+            return make_node<constant_node>(a ^ b);
+    }
+}
+
 constexpr inline void simplify(node& root_node, vm& vm)
 {
     const struct
@@ -21,39 +38,59 @@ constexpr inline void simplify(node& root_node, vm& vm)
         node& root_node;
         struct vm& vm;
 
-        constexpr auto operator()(op_node& op) const
+        constexpr auto operator()(op_node& root_op) const
         {
-            simplify(*op.left, vm);
-            simplify(*op.right, vm);
+            simplify(*root_op.left, vm);
+            simplify(*root_op.right, vm);
 
-            if (!std::holds_alternative<constant_node>(*op.left) || !std::holds_alternative<constant_node>(*op.right))
+            auto& vm2 = vm;
+
+            if (!std::holds_alternative<constant_node>(*root_op.left) || !std::holds_alternative<constant_node>(*root_op.right)) {
+                const auto constant_move = [&vm2](auto& root_node_to_check, auto& node_to_check, auto& other_side, auto& to_replace, auto op_type) {
+                    if (std::holds_alternative<constant_node>(*node_to_check)) {
+                        if (std::holds_alternative<constant_node>(*to_replace)) {
+                            to_replace = std::make_unique<node>(make_node<op_node>(std::move(node_to_check),
+                                                                                   std::move(to_replace),
+                                                                                   op_type));
+                            root_node_to_check = std::move(other_side);
+                            simplify(*to_replace, vm2);
+                            return true;
+                        }
+                        std::swap(node_to_check, to_replace);
+                        return true;
+                    }
+                    return false;
+                };
+
+                const auto constant_fold = [&constant_move](auto& root_node_to_check, auto& to_replace, auto root_op_type){
+                    if (std::holds_alternative<op_node>(*root_node_to_check)) {
+                        auto& checking_op = std::get<op_node>(*root_node_to_check);
+                        if (checking_op.type == root_op_type) {
+                            bool changed = constant_move(root_node_to_check, checking_op.left, checking_op.right, to_replace, checking_op.type);
+                            if (!changed)
+                                changed = constant_move(root_node_to_check, checking_op.right, checking_op.left, to_replace, checking_op.type);
+                            return changed;
+                        }
+                    }
+                    return false;
+                };
+
+                bool switched = constant_fold(root_op.right, root_op.left, root_op.type);
+                if (!switched)
+                    switched = constant_fold(root_op.left, root_op.right, root_op.type);
+
                 return;
-
-            const auto left_result_number = std::get<constant_node>(*op.left);
-            const auto right_result_number = std::get<constant_node>(*op.right);
-
-            switch(op.type) {
-                case operation_type::mul:
-                    root_node = make_node<constant_node>(left_result_number.value * right_result_number.value);
-                    break;
-                case operation_type::div:
-                    root_node = make_node<constant_node>(left_result_number.value / right_result_number.value);
-                    break;
-                case operation_type::add:
-                    root_node = make_node<constant_node>(left_result_number.value + right_result_number.value);
-                    break;
-                case operation_type::sub:
-                    root_node = make_node<constant_node>(left_result_number.value - right_result_number.value);
-                    break;
-                case operation_type::exp:
-                    root_node = make_node<constant_node>(left_result_number.value ^ right_result_number.value);
-                    break;
             }
+
+            const auto left_result_number = std::get<constant_node>(*root_op.left).value;
+            const auto right_result_number = std::get<constant_node>(*root_op.right).value;
+
+            root_node = operate(left_result_number, right_result_number, root_op.type);
         }
 
         constexpr auto operator()(constant_node&) const
         {
-            return;
+
         }
 
         constexpr auto operator()(symbol_node& symbol) const
